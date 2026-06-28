54bcf385cd04c7f9bfe42b8f73c06aa318bf0bca
Abhiram Sakha <abhiramsakhaa@gmail.com>
2026-06-28 15:32:46 +0530

    Initial Commit

--- Day68/Number of Group Form Problem.py
+++ Day68/Number of Group Form Problem.py
@@ -1,22 +1,33 @@
 def get_anagram_groups(strs):
     n = len(strs)
+    if n == 0:
+        return 0
+
     parent = list(range(n))
+    rank = [0] * n
 
     def find(x):
-        while parent[x] != x:
-            parent[x] = parent[parent[x]]
-            x = parent[x]
-        return x
+        if parent[x] != x:
+            parent[x] = find(parent[x])
+        return parent[x]
 
     def union(x, y):
-        px, py = find(x), find(y)
-        if px != py:
+        px = find(x)
+        py = find(y)
+        if px == py:
+            return
+        if rank[px] < rank[py]:
+            parent[px] = py
+        elif rank[px] > rank[py]:
             parent[py] = px
+        else:
+            parent[py] = px
+            rank[px] += 1
 
     def similar(a, b):
         diff = 0
-        for x, y in zip(a, b):
-            if x != y:
+        for i in range(len(a)):
+            if a[i] != b[i]:
                 diff += 1
                 if diff > 4:
                     return False
@@ -27,9 +38,18 @@ def get_anagram_groups(strs):
             if similar(strs[i], strs[j]):
                 union(i, j)
 
-    return len({find(i) for i in range(n)})
+    groups = set()
+    for i in range(n):
+        groups.add(find(i))
+
+    return len(groups)
+
 
 if __name__ == "__main__":
     n = int(input())
-    arr = input().split()
+
+    arr = []
+    while len(arr) < n:
+        arr.extend(input().split())
+
     print(get_anagram_groups(arr))
